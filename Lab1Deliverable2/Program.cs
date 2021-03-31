e6e3676043b555802dbe3cf1cddf31a920b64ec7
mdinicola <the.m.dinicola@gmail.com>
2021-03-31 19:48:03 -0400

    draft 1 no comments

--- Lab1Deliverable2/Program.cs
+++ Lab1Deliverable2/Program.cs
@@ -6,7 +6,30 @@ namespace Lab1Deliverable2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string oldIn = "";
+            do
+            {
+                Console.WriteLine("What do you wish to say to the bot?");
+                string inText = Console.ReadLine().ToLower();
+                if (inText.Equals(oldIn))
+                {
+                    Console.WriteLine("I'm sorry but you have already said that");
+                }
+                else if (inText.Equals("sup"))
+                {
+                    Console.WriteLine("I am good");
+                }
+                else if (inText.Equals("hello there"))
+                {
+                    Console.WriteLine("General Kenobi");
+                }
+                else if (inText.Equals("hello"))
+                {
+                    Console.WriteLine("Hi good to see you.");
+                }
+                oldIn = inText;
+            } while (oldIn != "bye");
+            Console.WriteLine("Good bye!");
         }
     }
 }
